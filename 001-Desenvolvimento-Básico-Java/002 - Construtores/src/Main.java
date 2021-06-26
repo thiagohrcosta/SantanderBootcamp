9170bbc11696624be093d10639aabd5e676c430a
Thiago Costa <thiagohrcosta86@gmail.com>
2021-06-26 16:19:20 -0300

    Trabalhando com construtores

---
+++ 001-Desenvolvimento-Básico-Java/002 - Construtores/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+  public static void main(String[] args) {
+
+    Pessoa pessoa = new Pessoa("John Doe");
+    System.out.println("O nome da pessoa é: " + pessoa.getNome());
+  }
+}
