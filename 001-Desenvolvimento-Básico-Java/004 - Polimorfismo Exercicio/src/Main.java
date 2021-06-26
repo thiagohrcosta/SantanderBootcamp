015ff7218cce3429cfb6bbf75aaec55fb3159dc3
Thiago Costa <thiagohrcosta86@gmail.com>
2021-06-26 17:49:43 -0300

    Fix typo

--- 001-Desenvolvimento-Básico-Java/004 - Polimorfismo Exercicio/src/Main.java
+++ 001-Desenvolvimento-Básico-Java/004 - Polimorfismo Exercicio/src/Main.java
@@ -2,7 +2,7 @@ public class Main {
   public static void main(String[] args) {
 
     Gerente gerente = new Gerente();
-    gerente.setNome("Apolo Creed");
+    gerente.setNome("Apollo Creed");
     gerente.setSalario(5000.0);
 
     Supervisor supervisor = new Supervisor();
