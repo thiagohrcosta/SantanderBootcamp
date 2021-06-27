6492141a0f786f1336149435ad791662dcd3d5bd
Thiago Costa <thiagohrcosta86@gmail.com>
2021-06-27 16:53:44 -0300

    Criando primeira lista com Angular

--- 008 - Introdução ao Angular 8/course-manager/src/app/courses/course-list.component.ts
+++ 008 - Introdução ao Angular 8/course-manager/src/app/courses/course-list.component.ts
@@ -1,10 +1,39 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { Course } from './course';
 
 @Component({
   selector: 'app-course-list',
   templateUrl: './course-list.component.html'
 })
 
-export class CourseListComponent {
+export class CourseListComponent implements OnInit {
+
+  courses: Course[] = [];
+
+    ngOnInit(): void {
+      this.courses = [
+        {
+          id: 1,
+          name: 'Angular: Forms',
+          imageUrl: '',
+          price: 99.99,
+          code: 'XPS-8996',
+          duration: 120,
+          rating: 4,
+          releaseDate: 'December, 2, 2020'
+        },
+        {
+          id: 2,
+          name: 'Angular: HTTP',
+          imageUrl: '',
+          price: 49.99,
+          code: 'XPS-8956',
+          duration: 90,
+          rating: 5,
+          releaseDate: 'December, 4, 2020'
+        }
+      ]
+    }
+
 
 }
