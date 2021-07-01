dde8ca4b43c08d940456cd0c3604e9ca90510409
Thiago Costa <thiagohrcosta86@gmail.com>
2021-06-30 22:46:34 -0300

    Trabalhando com rotas save e edit no Angular

--- 008 - Introdução ao Angular 8/course-manager/src/app/courses/course-info.component.ts
+++ 008 - Introdução ao Angular 8/course-manager/src/app/courses/course-info.component.ts
@@ -1,20 +1,26 @@
-import { Component, OnInit } from "@angular/core";
-import { ActivatedRoute } from "@angular/router";
+  import { Component, OnInit } from "@angular/core";
+  import { ActivatedRoute } from "@angular/router";
+import { Course } from "./course";
+  import { CourseService } from "./course.service";
 
-@Component({
-  templateUrl: './course-info.component.html'
-})
+  @Component({
+    templateUrl: './course-info.component.html'
+  })
 
-export class CourseInfoComponent implements OnInit {
+  export class CourseInfoComponent implements OnInit {
 
-  courseId: number = 0;
+    course!: Course;
 
-  constructor(private activatedRoute: ActivatedRoute){
+    constructor(private activatedRoute: ActivatedRoute, private courseService: CourseService){
+
+    }
+
+    ngOnInit(): void{
+      this.course = this.courseService.retrieveById(+this.activatedRoute.snapshot.paramMap.get('id')!)!;
+    }
+
+    save(): void{
+      this.courseService.save(this.course);
+    }
 
   }
-
-  ngOnInit(): void{
-    this.courseId = +this.activatedRoute.snapshot.paramMap.get('id')!;
-  }
-
-}
