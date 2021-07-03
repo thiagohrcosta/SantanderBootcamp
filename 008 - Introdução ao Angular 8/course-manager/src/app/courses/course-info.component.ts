35639e6080c0d898766b519fcf933f039f96520f
Thiago Costa <thiagohrcosta86@gmail.com>
2021-07-03 14:52:58 -0300

    Primeira parte HTTP com Angular

--- 008 - Introdução ao Angular 8/course-manager/src/app/courses/course-info.component.ts
+++ 008 - Introdução ao Angular 8/course-manager/src/app/courses/course-info.component.ts
@@ -1,7 +1,7 @@
-  import { Component, OnInit } from "@angular/core";
-  import { ActivatedRoute } from "@angular/router";
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
 import { Course } from "./course";
-  import { CourseService } from "./course.service";
+import { CourseService } from "./course.service";
 
   @Component({
     templateUrl: './course-info.component.html'
@@ -16,11 +16,17 @@ import { Course } from "./course";
     }
 
     ngOnInit(): void{
-      this.course = this.courseService.retrieveById(+this.activatedRoute.snapshot.paramMap.get('id')!)!;
+      this.courseService.retrieveById(+this.activatedRoute.snapshot.paramMap.get('id')!)!.subscribe({
+        next: course => this.course = course,
+        error: err => console.log("Error", err)
+      });
     }
 
     save(): void{
-      this.courseService.save(this.course);
+      this.courseService.save(this.course).subscribe({
+        next: course => console.log('Saved with success', course),
+        error: err => console.log("Error", err)
+      });
     }
 
   }
